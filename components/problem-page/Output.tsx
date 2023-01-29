e766822c85f7ee14318d41faec7fae9e341cd29e
javier-rosas <jr2dj@virginia.edu>
2023-01-29 17:38:59 -0500

    completed unit testing on 3 questions and fixed formatting issues

--- components/problem-page/Output.tsx
+++ components/problem-page/Output.tsx
@@ -9,13 +9,15 @@ function Output({ handleCompile, code, classnames, processing }: any) {
           maxWidth: "110vh",
           marginRight: 5,
         }}
-        className="bg-gray-800 mt-1 rounded-md text-white font-mono text-sm overflow-y-scroll overflow-x-auto p-3 max-h-24 "
+        className="bg-gray-800 mt-1 rounded-md text-white font-mono 
+                    text-sm overflow-y-auto overflow-x-auto p-3 max-h-24"
       />
       <button
         onClick={handleCompile}
         disabled={!code}
         className={classnames(
-          "mt-2 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0",
+          `mt-2 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] 
+           px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0`,
           !code ? "opacity-50" : ""
         )}
       >
